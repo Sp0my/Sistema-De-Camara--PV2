c78c0d5b6cb714fbc25f4a05ee62b49a805788c8
Juan Pablo Resendes Rosas <resendes.rosas614@gmail.com>
2023-02-26 00:46:01 -0600

    Main - Se realizaron cambios
    Primeros cambios al sistema de cámara

---
+++ CamaraSystem/Assets/CamaraProyect/CameraControl.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public bool activeTP;
+
+    public Transform posTP;
+    public Transform posPP;
+
+    //CAMARA TP
+    public float rotSpeed;
+    public float rotMin, rotMax;
+    float mouseX, mouseY;
+    public Transform target, player;
+
+
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void Cam()
+    {
+        mouseX += rotSpeed * Input.GetAxis("Mouse X");
+        mouseY -= rotSpeed * Input.GetAxis("Mouse Y");
+        mouseY = Mathf.Clamp(mouseY, rotMin, rotMax);
+
+        target.rotation = Quaternion.Euler(mouseY, mouseX, 0.0f);
+        player.rotation = Quaternion.Euler(0.0f, mouseX, 0.0f);
+    }
+
+    private void LateUpdate()
+    {
+        Cam();
+
+        if (activeTP == false && Input.GetKeyDown(KeyCode.Tab))
+        {
+            activeTP = true;
+            transform.position = posPP.position;
+        }
+        else if(activeTP == true && Input.GetKeyDown(KeyCode.Tab))
+        {
+            activeTP = false;
+            transform.position = posTP.position;
+            transform.LookAt(player);
+        }
+    }
+}
